2a93f45d98521a70186a59fc3201bba42ae0fb49
milindkumar64 <milindgupta@gmail.com>
2023-04-06 16:36:36 +0530

    Add: Add new Elements in a node

--- src/com/bridgelabz/LinkedListProblem.java
+++ src/com/bridgelabz/LinkedListProblem.java
@@ -7,11 +7,12 @@ class LinkedListProblem {
 	public static void main(String[] args) {
 
 		LinkedList<Integer> list = new LinkedList<Integer>();
-		
-		list.add(56);
-		list.add(30);
+			
 		list.add(70);
+		list.add(0,30);
+		list.add(0,56);
 
+		
 		System.out.println(list);
 		
 	}
