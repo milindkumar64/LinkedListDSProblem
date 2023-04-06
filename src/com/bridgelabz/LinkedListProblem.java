dc25769b2db79aad5237dd61ebb5a410ac7245cc
milindkumar64 <milindgupta@gmail.com>
2023-04-06 16:29:03 +0530

    Add: Create A simple LinkedList

---
+++ src/com/bridgelabz/LinkedListProblem.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+import java.util.*;
+
+class LinkedListProblem {
+
+	public static void main(String[] args) {
+
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		
+		list.add(56);
+		list.add(30);
+		list.add(70);
+
+		System.out.println(list);
+		
+	}
+}
