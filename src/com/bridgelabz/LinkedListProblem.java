429d80a0ab9ead234ae04fd4659e1f52df6f8382
milindkumar64 <milindgupta@gmail.com>
2023-04-07 12:59:54 +0530

    Add: Search an element in LinkedList and add in middle using listIterator

--- src/com/bridgelabz/LinkedListProblem.java
+++ src/com/bridgelabz/LinkedListProblem.java
@@ -13,14 +13,17 @@ class LinkedListProblem {
 		linkedlist.add(56);
 		linkedlist.add(30);
 		linkedlist.add(70);
+		
+		System.out.println(linkedlist);
 
-
-		Iterator itr = linkedlist.iterator();
-		while(itr.hasNext())
-		{
-			Integer i = (Integer)itr.next();
-			if(i==30)
-		    System.out.println("Got the Node with value 30");
+		ListIterator itr = linkedlist.listIterator();
+		
+		while (itr.hasNext()) {
+			Integer i = (Integer) itr.next();
+			if (i == 30)
+				{itr.add(40);
+				System.out.println("Got the Node with value 30 and adding Next Node 40");
+				}
 		}
 		System.out.println(linkedlist);
 
